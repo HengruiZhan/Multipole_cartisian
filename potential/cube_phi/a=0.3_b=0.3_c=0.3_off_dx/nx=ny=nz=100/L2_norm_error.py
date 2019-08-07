483bed2754cdc6679969f0e283b9df2e17313af2
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-08-07 02:39:15 -0400

    Create L2_norm_error.py

---
+++ potential/cube_phi/a=0.3_b=0.3_c=0.3_off_dx/nx=ny=nz=100/L2_norm_error.py
@@ -0,0 +1,15 @@
+# L2 norm error of cube
+# the sites of the cube are a = b = c =0.3
+# the number of grid point nx = ny = nz = 100
+# xlim = (-1.0, 1.0)
+# ylim = (-1.0, 1.0)
+# zlim = (-1.0, 1.0)
+# evaluate at the grid cell center face with x_grid_face = x_grid_center - dx/2
+
+lmax = 0, L2normerr = 0.004129259053218379
+lmax = 1, L2normerr = 0.003912801608538426
+lmax = 2, L2normerr = 0.00391070661969043
+lmax = 3, L2normerr = 0.003913627885664768
+lmax = 4, L2normerr = 0.003913372974168947
+lmax = 5, L2normerr = 0.003913363646001866
+lmax = 6, L2normerr = 0.003913366211304788
